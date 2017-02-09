da9694afc9cea3593418ce52a44add25448035c8
Ewan <ewan.le-goallec6@etu.univ-lorraine.fr>
2017-02-09 15:45:39 +0100

    Méthode afficherGroupe dans le controleur gestion

--- src/collocation/controleurs/ControleurGestion.php
+++ src/collocation/controleurs/ControleurGestion.php
@@ -20,4 +20,20 @@ class ControleurGestion
         }
     }
 
+    public function afficherGroupe(){
+        if(isset($_SESSION['email']) && isset($_SESSION['idGroupe'])){ // utilisateur connu
+            $user = User::where("email","=",$_SESSION['email'])->first();
+            if($user->estGestionnaire()){ // deja gerant
+                $valeur = Groupe::where("idGroupe","=",$_SESSION['idGroupe'])->first();
+                $vue = new VueNavigation($valeur);
+                //print $vue-> render(VueGestion::AFF_GROUPE);  // NOT YET IMPLEMENTED
+            }else{ // pas encore gerant
+
+            }
+        }else{ // utilisateur inconnu
+            $app = \Slim\Slim::getInstance();
+            $app->redirect("accueil");
+        }
+    }
+
 }
