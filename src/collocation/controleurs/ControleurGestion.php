788256656c7bd364f3f954dbffc1c88165323233
AMSLER Ophélien <ophelien.amsler7@etu.univ-lorraine.fr>
2017-02-09 15:10:43 +0100

    modifications

--- src/collocation/controleurs/ControleurGestion.php
+++ src/collocation/controleurs/ControleurGestion.php
@@ -2,7 +2,21 @@
 
 namespace collocation\controleurs;
 
+use \collocation\models\User;
+
 class ControleurGestion
 {
+    public function creerGroupe(){
+        if(isset($_SESSION['email'])){ // utilisateur connu
+            $user = User::where("email","=",$_SESSION['email'])->first();
+            if($user->estGestionnaire()){ // deja gerant
+
+            }else{ // pas encore gerant
+
+            }
+        }else{ // utilisateur inconnu
+
+        }
+    }
 
 }
--- src/collocation/models/User.php
+++ src/collocation/models/User.php
@@ -2,9 +2,21 @@
 
 namespace collocation\models;
 
+use \collocation\models\Appartient;
+
 class User extends \Illuminate\Database\Eloquent\Model {
     protected $table = "user";
     protected $primaryKey = "email";
     public $timestamps = false;
+
+    public function estGerant(){
+        $appartients = Appartient::where("email","=",$this->email)->get();
+        foreach ($appartients as $appartient){
+            if($appartient->urlGestion != null){
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
