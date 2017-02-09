c88bb0c37365257f8b5b0d0f13dae04b85905fa4
Ewan <ewan.le-goallec6@etu.univ-lorraine.fr>
2017-02-09 12:43:51 +0100

    mes début en php youhouuuuuu Ajout du début de la fonctionnalité 3

--- src/collocation/controleurs/ControleurNavigation.php
+++ src/collocation/controleurs/ControleurNavigation.php
@@ -3,6 +3,7 @@
 namespace collocation\controleurs;
 
 use \collocation\vues\VueNavigation;
+use \collocation\models\User;
 
 class Navigation
 {
@@ -10,6 +11,12 @@ class Navigation
     public function saisiLogin(){
         $vue = new VueNavigation();
         print $vue->render(VueNavigation::AFF_FORMULAIRE_LOGIN);
+    }
+
+    public function afficherListeUtilisateurs(){
+        $valeur = User::all();
+        $vue = new VueNavigation($valeur);
+        print $vue-> render(VueNavigation::AFF_LISTE_UTILISATEUR);
     }
 
 }
--- src/collocation/vues/VueNavigation.php
+++ src/collocation/vues/VueNavigation.php
@@ -5,6 +5,7 @@ namespace collocation\vues;
 class VueNavigation
 {
     const AFF_FORMULAIRE_LOGIN = 1;
+    const AFF_LISTE_UTILISATEUR = 2;
 
     private $objet;
 
@@ -19,7 +20,21 @@ class VueNavigation
             case VueNavigation::AFF_FORMULAIRE_LOGIN :
                 $content = $this->formulaire();
                 break;
+            case VueNavigation::AFF_LISTE_UTILISATEUR :
+                $content = $this->listeUtilisateur();
+                break;
         }
         return $content;
+    }
+
+    private function listeUtilisateur(){
+        $retour = "";
+        foreach($this->objet as $utilisateur){
+            $retour .= <<<end
+<p>$utilisateur->nom</p>
+<href="">
+end;
+        }
+        return $retour;
     }
 }
