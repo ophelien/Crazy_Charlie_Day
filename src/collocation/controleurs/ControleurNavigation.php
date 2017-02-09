f7d712472c711dbbbfacbe4573656949bd5b015b
AMSLER Ophélien <ophelien.amsler7@etu.univ-lorraine.fr>
2017-02-09 20:09:02 +0100

    ajout de l'image

--- src/collocation/controleurs/ControleurNavigation.php
+++ src/collocation/controleurs/ControleurNavigation.php
@@ -121,7 +121,7 @@ class ControleurNavigation
         if (sizeof ( $error ) == 0){
             print $_SERVER['REQUEST_URI'];
             //copy("sql.sql","sql.jpg");
-            copy("/img/user/defaut.jpg","/img/user/$mail.jpg");
+            copy("./img/user/defaut.jpg","./img/user/$mail.jpg");
             $mdp = password_hash($mdp, PASSWORD_DEFAULT, Array('cost' => 12));
             $u = new User();
             $u->email = $mail;
