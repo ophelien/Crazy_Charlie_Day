14689b5c0a2e3ea8b5d7cbc36bf9e49b5816adde
Ewan <ewan.le-goallec6@etu.univ-lorraine.fr>
2017-02-09 19:08:58 +0100

    pb pour ophélien

--- src/collocation/controleurs/ControleurGestionnaire.php
+++ src/collocation/controleurs/ControleurGestionnaire.php
@@ -12,4 +12,12 @@ namespace collocation\controleurs;
 class ControleurGestionnaire
 {
 
+    public function afficherTousLesGroupesComplets(){
+        if(isset($_SESSION['admin'])) { //si l'admin est vérifié
+            $groupe = Groupe::where('status','=',1)->get();
+        }else{ // si l'admin est inconnu
+
+        }
+        return $groupe;
+    }
 }
