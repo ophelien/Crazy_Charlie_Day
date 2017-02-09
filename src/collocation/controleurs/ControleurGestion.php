8bddf16b5d56faf7462086735e7dd20f79e796b5
AMSLER Ophélien <ophelien.amsler7@etu.univ-lorraine.fr>
2017-02-09 16:15:48 +0100

    modification

--- src/collocation/controleurs/ControleurGestion.php
+++ src/collocation/controleurs/ControleurGestion.php
@@ -2,6 +2,8 @@
 
 namespace collocation\controleurs;
 
+use collocation\models\Appartient;
+use collocation\models\Groupe;
 use \collocation\models\User;
 
 class ControleurGestion
@@ -10,9 +12,19 @@ class ControleurGestion
         if(isset($_SESSION['email'])){ // utilisateur connu
             $user = User::where("email","=",$_SESSION['email'])->first();
             if($user->estGestionnaire()){ // deja gerant
-
+                $app = \Slim\Slim::getInstance();
+                $app->redirect("accueil");
             }else{ // pas encore gerant
-
+                $groupe = new Groupe();
+                $groupe->status = 0;
+                $groupe->save();
+                $appartient = new Appartient();
+                $appartient->email = $_SESSION['email'];
+                $appartient->idGroupe = $groupe->idGroupe;
+                $appartient->estOk = 1;
+                $appartient->urlGestion = ""; // TODO
+                $appartient->save();
+                $this->afficherGroupe();
             }
         }else{ // utilisateur inconnu
             $app = \Slim\Slim::getInstance();
@@ -28,7 +40,8 @@ class ControleurGestion
                 $vue = new VueNavigation($valeur);
                 //print $vue-> render(VueGestion::AFF_GROUPE);  // NOT YET IMPLEMENTED
             }else{ // pas encore gerant
-
+                $app = \Slim\Slim::getInstance();
+                $app->redirect("accueil");
             }
         }else{ // utilisateur inconnu
             $app = \Slim\Slim::getInstance();
